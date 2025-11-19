ca3aecc456fb11b6e2dd5c7e81ef87f00a14310b
Virendra Gupta <virendragupta7@gmail.com>
2025-11-19 15:54:36 +0530

    added scroll view with vstack and hstack

--- iOS/TakeHome/TakeHome1/TakeHome1/ContentView.swift
+++ iOS/TakeHome/TakeHome1/TakeHome1/ContentView.swift
@@ -11,8 +11,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-  
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(1...20, id: \.self) { index in
+                    Text("Item \(index)")
+                        .padding()
+                        .background(Color.green.opacity(0.3))
+                        .cornerRadius(8)
+                }
+            }
+            .padding()
+        }
+        ScrollView() {
+            VStack {
+                ForEach(1...20, id: \.self) { index in
+                    Text("Item \(index)")
+                        .font(.headline)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue.opacity(0.2))
+                        .cornerRadius(8)
+                }
+            }
+        }
+        .padding()
+
+      Spacer()
     }
+       
+    
 }
 
 #Preview {
