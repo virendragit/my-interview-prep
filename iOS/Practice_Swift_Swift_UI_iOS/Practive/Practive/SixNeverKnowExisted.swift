e5ff5b0ad4e3dbe63ff0504b31f9f79556725409
Virendra Gupta <virendragupta7@gmail.com>
2025-11-25 17:17:55 +0530

    updated logic for hr min sec

--- iOS/Practice_Swift_Swift_UI_iOS/Practive/Practive/SixNeverKnowExisted.swift
+++ iOS/Practice_Swift_Swift_UI_iOS/Practive/Practive/SixNeverKnowExisted.swift
@@ -10,17 +10,32 @@ import SwiftUI
 struct SixNeverKnowExisted: View {
     var body: some View {
         
-        VStack {
+        VStack(spacing: 10 ) {
 
-            let endDate = Date().addingTimeInterval(30)
+            let endDate = Date().addingTimeInterval(300)
 
             TimelineView(.periodic(from: .now, by: 1)) { context in
                            let remaining = max(Int(endDate.timeIntervalSince(context.date)), 0)
-                           Text("\(remaining)s left")
-                               .font(.system(size: 36, weight: .bold, design: .rounded))
-                               .foregroundColor(remaining == 0 ? .red : .green)
-                               .animation(.easeInOut, value: remaining)
+//                           Text("\(remaining)s left")
+//                               .font(.system(size: 36, weight: .bold, design: .rounded))
+//                               .foregroundColor(remaining == 0 ? .red : .green)
+//                               .animation(.easeInOut, value: remaining)
+                let hours = remaining / 3600
+                               let minutes = (remaining % 3600) / 60
+                               let seconds = remaining % 60
+
+                               Text(String(format: "%02d:%02d:%02d left", hours, minutes, seconds))
+                                   .font(.system(size: 36, weight: .bold, design: .rounded))
+                                   .foregroundColor(remaining == 0 ? .red : .green)
+                                   .animation(.easeInOut, value: remaining)
                        }
+//            Spacer(minLength: 10)
+            TimelineView(.animation){context in
+                Text(context.date,style: .time)
+                    .font(.system(size: 36, weight: .bold, design: .rounded))
+            }
+//            .padding(.top, 20)
+            Spacer()
             }
     }
 }
